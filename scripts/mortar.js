bd103790c1493cc33159af21efa973af821e3fdc
Thomas.V <verhille.thomas@orange.fr>
2020-04-29 21:50:13 +0200

    mortar shot upgraded

--- scripts/mortar.js
+++ scripts/mortar.js
@@ -5,40 +5,48 @@ const mortarLaunchEffect = newEffect(20, e => {
     Lines.circle(e.x, e.y, e.fin() * 30); //draw a circle whose radius goes from 0 to 30
 });
 
+const listBulletFrag1 = [];
+const listMortarBullet = [];
 
-
-const testBullet = new ArtilleryBulletType(2.5, 10, "shell");
-const fragBullet1 =  new BombBulletType(7, 10, "shell");
 const fragBullet2 =  new LiquidBulletType(Liquids.oil);
 const fragBullet3 =  new LiquidBulletType(Liquids.slag);
 
-testBullet.damage = 275;
-testBullet.bulletWidth = 18;
-testBullet.bulletHeight = 22;
-testBullet.bulletShrink = 0.8;
-testBullet.splashDamageRadius= 12;
-testBullet.splashDamage= 160;
-testBullet.ammoMultiplier = 1;
-testBullet.fragBullets = 15;
-testBullet.fragBullet = fragBullet1;
-
-fragBullet1.fragBullets = 1;
-fragBullet1.damage = 50;
-fragBullet1.splashDamage = 20;
-fragBullet1.splashDamageRadius = 4;
-fragBullet1.fragBullet = fragBullet2;
-
 fragBullet2.lifetime=2;
 fragBullet2.speed = 1;
-fragBullet2.damage = 15;
-fragBullet2.statusDuration = 300;
-fragBullet2.fragBullets = 2;
+fragBullet2.damage = 30;
+fragBullet2.statusDuration = 600;
+fragBullet2.fragBullets = 3;
 fragBullet2.fragBullet = fragBullet3;
 
 fragBullet3.lifetime=2;
 fragBullet3.speed = 1;
-fragBullet3.damage = 5;
-fragBullet3.statusDuration = 300;
+fragBullet3.damage = 15;
+fragBullet3.statusDuration = 450;
+
+for (var i = 0; i < 50; i++) {
+    listBulletFrag1[i] = new BombBulletType(1, 1, "shell"); 
+    listBulletFrag1[i].speed = Math.random()*2.6+0.7;
+    listBulletFrag1[i].lifetime = Math.random()*35+25
+    print("bullet number : "+ i +", speed = "+ listBulletFrag1[i].speed + ", lifetime = "+listBulletFrag1[i].lifetime);
+    listBulletFrag1[i].fragBullets = 3;
+    listBulletFrag1[i].damage = 70;
+    listBulletFrag1[i].splashDamage = 50;
+    listBulletFrag1[i].splashDamageRadius = 12;
+    listBulletFrag1[i].fragBullet = fragBullet2;
+};
+
+for (var i = 0; i < 50; i++) {
+    listMortarBullet[i] = new ArtilleryBulletType(1,1, "shell");
+    listMortarBullet[i].damage = 425/50;
+    listMortarBullet[i].bulletWidth = 18;
+    listMortarBullet[i].bulletHeight = 22;
+    listMortarBullet[i].bulletShrink = 0.8;
+    listMortarBullet[i].splashDamageRadius= 36;
+    listMortarBullet[i].splashDamage= 280/50;
+    listMortarBullet[i].ammoMultiplier = 1;
+    listMortarBullet[i].fragBullets = 1;
+    listMortarBullet[i].fragBullet = listBulletFrag1[i];
+};
 
 const mortar = extendContent(Block, "mortar", {
     buildConfiguration(tile, table){
@@ -63,8 +71,9 @@ const mortar = extendContent(Block, "mortar", {
 
             if(yShip - tile.y < 0) angle *= -1;
 
-            //create 1 bullet
-            Calls.createBullet(testBullet, tile.getTeam(), tile.drawx(), tile.drawy(), angle, 0.4, 4.5)
+            for(var i = 0; i< listMortarBullet.length; i++){
+                Calls.createBullet(listMortarBullet[i], tile.getTeam(), tile.drawx(), tile.drawy(), angle, 0.95, 4.5)
+            }
             tile.entity.cons.trigger()
         }
     }
