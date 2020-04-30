8e18e34dfdb6a05d2c31695ee1566b9b8783881a
Thomas.V <verhille.thomas@orange.fr>
2020-04-30 19:43:30 +0200

    can lock a direction

--- scripts/mortar.js
+++ scripts/mortar.js
@@ -49,31 +49,64 @@ for (var i = 0; i < 50; i++) {
 
 const mortar = extendContent(Block, "mortar", {
     buildConfiguration(tile, table){
+        this.mortarList.forEach(mortar => {
+            if(mortar[0] == tile.x && mortar[1] == tile.y) rightMortar = mortar;
+        })
         table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            tile.configure(0)
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+            tile.configure(1)
+        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid() || (new Date().getTime() - rightMortar[4]) < 1000 ))
+        table.addImageButton(Icon.lock, Styles.clearTransi, run(() => {
+            tile.configure(2)
+        })).size(50).disabled(boolf(b => tile.entity == null))
     },
 
-    configured(tile, value){
-        if(tile.entity.cons.valid()){
-            Effects.effect(mortarLaunchEffect, tile)
+    doSomeMath(tile, player){
+        xShip = player.x/8;
+        yShip = player.y/8;
+        valueA = xShip - tile.x;
+        valueB = Math.sqrt( (xShip - tile.x)*(xShip - tile.x) + (yShip - tile.y)*(yShip - tile.y) );
 
-            //do math to known angle
-            xShip = value.x/8;
-            yShip = value.y/8;
-            valueA = xShip - tile.x;
-            valueB = Math.sqrt( (xShip - tile.x)*(xShip - tile.x) + (yShip - tile.y)*(yShip - tile.y) );
+        cosAlpha = valueA / valueB;
 
-            cosAlpha = valueA / valueB;
+        angle = Math.acos(cosAlpha)*180/Math.PI;
 
-            angle = Math.acos(cosAlpha)*180/Math.PI;
+        if(yShip - tile.y < 0) angle *= -1;
 
-            if(yShip - tile.y < 0) angle *= -1;
+        return angle;
+    },
 
-            for(var i = 0; i< listMortarBullet.length; i++){
-                Calls.createBullet(listMortarBullet[i], tile.getTeam(), tile.drawx(), tile.drawy(), angle, 0.95, 4.5)
+    //[mortarX, mortarY, locked, angle, last shoot]
+    mortarList : [],
+
+    configured(tile, player, value){
+        if(value == 0){ //only happen when placed.
+            this.mortarList.push([tile.x, tile.y, false, 0, 0])
+        }
+        else{
+
+            this.mortarList.forEach(mortar => {
+                if(mortar[0] == tile.x && mortar[1] == tile.y) rightMortar = mortar;
+            })
+
+            if (value == 1 && tile.entity.cons.valid()){
+                Effects.effect(mortarLaunchEffect, tile)
+
+                //do math to known angle
+                if(!rightMortar[2]) angle = this.doSomeMath(tile, player);
+
+                for(var i = 0; i< listMortarBullet.length; i++){
+                    if (rightMortar[2]) Calls.createBullet(listMortarBullet[i], tile.getTeam(), tile.drawx(), tile.drawy(), rightMortar[3], 0.95, 4.5);
+                    else Calls.createBullet(listMortarBullet[i], tile.getTeam(), tile.drawx(), tile.drawy(), angle, 0.95, 4.5)
+                }
+                rightMortar[4] =  new Date().getTime();
+                tile.entity.cons.trigger()
             }
-            tile.entity.cons.trigger()
+
+            else if(value == 2){
+                if(rightMortar[2]) rightMortar[2] = false;
+                else rightMortar[2] = true;
+                rightMortar[3] = this.doSomeMath(tile, player);
+            }
         }
     }
 })
